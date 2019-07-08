780574f8d0725b93645e6962edd800499bf2e9ad
viloria <viloria_f@yahoo.com.mx>
2019-07-08 17:25:14 -0500

    48. Optimizaciones para la aplicacion de notas

--- config/yargs.js
+++ config/yargs.js
@@ -1,28 +1,24 @@
+const descripcion = {
+    demand: true, //obligatorio
+    alias: 'd',
+    desc: 'Descripcion de la tarea por hacer'
+};
+const completado = {
+    default: true, //valor por default
+    alias: 'c',
+    desc: 'Marca como completado o pendiente la tarea'
+}
+
 const argv = require('yargs') //requiero el paquete YARGS
     .command('crear', 'crear un elemento por hacer', {
-        descripcion: {
-            demand: true,
-            alias: 'd'
-        }
+        descripcion: descripcion
     })
     .command('actualizar', 'actualiza el estado completado de una tarea', {
-        descripcion: {
-            demand: true, //obligatorio
-            alias: 'd',
-            desc: 'Descripcion de la tarea por hacer'
-        },
-        completado: {
-            default: true, //valor por default
-            alias: 'c',
-            desc: 'Marca como completado o pendiente la tarea'
-        }
+        descripcion,
+        completado
     })
     .command('borrar', 'borra una tarea que envian con el parametro descripcion', {
-        descripcion: {
-            demand: true,
-            alias: 'd',
-            desc: 'borra tarea'
-        }
+        descripcion
     })
     .help()
     .argv; //regresar el argv
